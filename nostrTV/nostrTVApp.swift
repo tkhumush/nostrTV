d371556fd632400b4516e41cb673e87ee606c770
Freedom Smuggler <134819643+tkhumush@users.noreply.github.com>
2025-04-24 20:55:43 -0400

    Initial Commit

---
+++ nostrTV/nostrTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  nostrTVApp.swift
+//  nostrTV
+//
+//  Created by Taymur Khumush on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct nostrTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nostrTVTests/nostrTVTests.swift
@@ -0,0 +1,17 @@
+//
+//  nostrTVTests.swift
+//  nostrTVTests
+//
+//  Created by Taymur Khumush on 4/24/25.
+//
+
+import Testing
+@testable import nostrTV
+
+struct nostrTVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
