df806c5936daf0a1dced1564263bf43886cc21be
Freedom Smuggler <134819643+tkhumush@users.noreply.github.com>
2025-05-01 23:05:07 -0400

    VideoPlayerUpdates
    - Ignore Screensaver.
    - Add logo in player.

--- nostrTV/VideoPlayerView.swift
+++ nostrTV/VideoPlayerView.swift
@@ -12,11 +12,36 @@ struct VideoPlayerView: UIViewControllerRepresentable {
     let player: AVPlayer
 
     func makeUIViewController(context: Context) -> AVPlayerViewController {
-        let controller = AVPlayerViewController()
+        let controller = CustomAVPlayerViewController()
         controller.player = player
+        // Add logo overlay
+        let logoImageView = UIImageView(image: UIImage(named: "Logo"))
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.contentMode = .scaleAspectFit
+        controller.contentOverlayView?.addSubview(logoImageView)
+
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: controller.view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            logoImageView.trailingAnchor.constraint(equalTo: controller.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+            logoImageView.heightAnchor.constraint(equalToConstant: 50)
+        ])
         controller.player?.play()
         return controller
     }
 
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {}
+}
+
+// Custom controller that disables the idle timer
+class CustomAVPlayerViewController: AVPlayerViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
 }
